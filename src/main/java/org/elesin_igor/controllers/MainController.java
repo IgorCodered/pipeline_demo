ee96586de8bfdc74bf1d9af5458da8bc038ab657
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2025-04-02 13:37:47 +0700

    Init repo

---
+++ src/main/java/org/elesin_igor/controllers/MainController.java
@@ -0,0 +1,24 @@
+package org.elesin_igor.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Базовый контроллер
+ */
+
+@RestController
+@RequestMapping("/api")
+public class MainController {
+
+	/**
+	 * Первый рест контроллер
+	 * @return 100
+	 */
+
+	@GetMapping("/getTask")
+	public int getTask() {
+		return 100;
+	}
+}
---
+++ src/main/java/org/elesin_igor/pipeline_demo/package-info.java
@@ -0,0 +1,1 @@
+package org.elesin_igor.pipeline_demo;
