5c14eca84bf9d8478fc25a39ba96935dc62d58f8
Igor Elesin <149936179+IgorCodered@users.noreply.github.com>
2025-04-02 14:40:16 +0700

    Init repo 2

--- src/main/java/org/elesin_igor/controllers/MainController.java
+++ src/main/java/org/elesin_igor/controllers/MainController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * Базовый контроллер
+ * Базовый контроллер хеллоу
  */
 
 @RestController
